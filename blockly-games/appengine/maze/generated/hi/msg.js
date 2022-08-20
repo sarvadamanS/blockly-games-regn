4bf764d78e24d03123992b9a7e70d3fc98afc7aa
sarvadamanS <sarvadamansingh03@gmail.com>
2022-08-20 17:16:12 +0530

    first commit

---
+++ blockly-games/appengine/maze/generated/hi/msg.js
@@ -0,0 +1,42 @@
+// This file was automatically generated.  Do not modify.
+
+'use strict';
+
+goog.provide('BlocklyGames.Msg');
+goog.require('Blockly.Msg');
+
+Blockly.Msg["ADD_COMMENT"] = "ટિપ્પણી ઉમેરો";
+Blockly.Msg["CANNOT_DELETE_VARIABLE_PROCEDURE"] = "ચલ '%1' કાઢી શકાતું નથી કારણ કે તે '%2' ફંક્શનની વ્યાખ્યાનો ભાગ છે";
+Blockly.Msg["CLEAN_UP"] = "બ્લોક્સ સાફ કરો";
+Blockly.Msg["COLLAPSE_ALL"] = "બ્લૉક્સ સંકુચિત કરો";
+Blockly.Msg["COLLAPSE_BLOCK"] = "બ્લોક સંકુચિત કરો";
+Blockly.Msg["DELETE_ALL_BLOCKS"] = "બધા %1 બ્લોક કાઢી નાખો?";
+Blockly.Msg["DELETE_BLOCK"] = "બ્લોક કાઢી નાખો";
+Blockly.Msg["DELETE_VARIABLE_CONFIRMATION"] = "'%2' વેરીએબલના %1 ઉપયોગો કાઢી નાખો?";
+Blockly.Msg["DELETE_X_BLOCKS"] = "%1 બ્લોક્સ કાઢી નાખો";
+Blockly.Msg["DIALOG_CANCEL"] = "રદ કરો";
+Blockly.Msg["DIALOG_OK"] = "ઓકે ભાઈ";
+Blockly.Msg["DISABLE_BLOCK"] = "બ્લોક અક્ષમ કરો";
+Blockly.Msg["DUPLICATE_BLOCK"] = "ડુપ્લિકેટ";
+Blockly.Msg["DUPLICATE_COMMENT"] = "ડુપ્લિકેટ ટિપ્પણી";
+Blockly.Msg["ENABLE_BLOCK"] = "બ્લોક સક્ષમ કરો";
+Blockly.Msg["EXPAND_ALL"] = "બ્લોક વિસ્તૃત કરો";
+Blockly.Msg["EXPAND_BLOCK"] = "બ્લોક વિસ્તૃત કરો";
+Blockly.Msg["EXTERNAL_INPUTS"] = "બાહ્ય ઇનપુટ્સ";
+Blockly.Msg["HELP"] = "સહાય";
+Blockly.Msg["INLINE_INPUTS"] = "ઇનલાઇન ઇનપુટ્સ";
+Blockly.Msg["NEW_VARIABLE"] = "ચલ બનાવો...";
+Blockly.Msg["NEW_VARIABLE_TITLE"] = "નવું ચલ નામ:";
+Blockly.Msg["PROCEDURES_DEFNORETURN_PROCEDURE"] = "કંઈક કરો";
+Blockly.Msg["REDO"] = "ફરીથી કરો";
+Blockly.Msg["REMOVE_COMMENT"] = "ટિપ્પણી દૂર કરો";
+Blockly.Msg["RENAME_VARIABLE"] = "ચલનું નામ બદલો...";
+Blockly.Msg["RENAME_VARIABLE_TITLE"] = "તમામ '%1' ચલોનું નામ બદલો:";
+Blockly.Msg["UNDO"] = "પૂર્વવત્ કરો";
+Blockly.Msg["UNNAMED_KEY"] = "અનામી";
+Blockly.Msg["VARIABLE_ALREADY_EXISTS"] = "'%1' નામનું ચલ પહેલેથી જ અસ્તિત્વમાં છે.";
+Blockly.Msg["VARIABLE_ALREADY_EXISTS_FOR_ANOTHER_TYPE"] = "'%1' નામનું ચલ પહેલાથી જ બીજા પ્રકાર માટે અસ્તિત્વમાં છે: '%2'.";
+Blockly.Msg["WORKSPACE_ARIA_LABEL"] = "અવરોધિત વર્કસ્પેસ";
+Blockly.Msg["WORKSPACE_COMMENT_DEFAULT_TEXT"] = "કંઈક કહો...";
+Blockly.Msg["PROCEDURES_DEFRETURN_PROCEDURE"] = Blockly.Msg["PROCEDURES_DEFNORETURN_PROCEDURE"];
+
